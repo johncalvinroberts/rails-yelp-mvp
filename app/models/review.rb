142c6e5366d1e8c267e4f70bb0a6ef610d308588
John Roberts <john.calvin.roberts@gmail.com>
2017-03-15 18:46:31 +0800

    FUC

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   validates :content, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true }, :inclusion => { :in => (0..5)}
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: (0..5)}
   belongs_to :restaurant
 end
